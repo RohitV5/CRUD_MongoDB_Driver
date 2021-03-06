1872755a1d2b0ec3c9d071f46526c09f5be11c8e
Rohit Verma <rohit.verma03@nagarro.com>
2021-03-06 13:02:02 +0530

    rendering element from arrays

--- src/App.js
+++ src/App.js
@@ -7,8 +7,20 @@ class App extends Component {
       super();
 
       this.state = {
-        string:"Hello",
-        text: "Edit <code>src/App.js</code> and save to reload ya."
+        monsters:[{
+          name: ' Frankenstein',
+          id: 'asc1'
+        },
+        {
+          name: ' Dracula',
+          id:'asc2'
+        },
+        {
+          name: ' Zombie',
+          id:'asc3'
+        }
+
+        ]
       }
     }
     
@@ -19,23 +31,9 @@ class App extends Component {
       render () {
         return (
           <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                {this.state.text}
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-              <button onClick={()=>this.changeStuff()}>
-                {this.state.string}
-              </button>
-            </header>
+           {
+             this.state.monsters.map(monster => <h1 key={monster.id}> { monster.name } </h1>)
+           }
           </div>
         );
       }   
